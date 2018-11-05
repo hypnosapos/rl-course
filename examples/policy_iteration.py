3e9dc126973ae9567a195a00f539d4aebb3819dd
David Suarez <david.suarez@intelygenz.com>
2018-11-05 15:10:10 +0100

    Policy & Value Iteration Examples

--- examples/policy_iteration.py
+++ examples/policy_iteration.py
@@ -24,31 +24,31 @@ def run_episode(env, policy, gamma = 1.0, render = False):
 
 
 def evaluate_policy(env, policy, gamma = 1.0, n = 100):
-    scores = [run_episode(env, policy, gamma, False) for _ in range(n)]
+    scores = [run_episode(env, policy, gamma, True) for _ in range(n)]
     return np.mean(scores)
 
-def extract_policy(v, gamma = 1.0):
+def improve_policy(v, gamma = 1.0):
     """ Extract the policy given a value-function """
-    policy = np.zeros(env.nS)
-    for s in range(env.nS):
-        q_sa = np.zeros(env.nA)
-        for a in range(env.nA):
-            q_sa[a] = sum([p * (r + gamma * v[s_]) for p, s_, r, _ in  env.P[s][a]])
+    policy = np.zeros(env.observation_space.n)
+    for s in range(env.observation_space.n):
+        q_sa = np.zeros(env.action_space.n)
+        for a in range(env.action_space.n):
+            q_sa[a] = sum([p * (r + gamma * v[s_]) for p, s_, r, _ in  env.env.P[s][a]])
         policy[s] = np.argmax(q_sa)
     return policy
 
-def compute_policy_v(env, policy, gamma=1.0):
+def evaluate_policy_v(env, policy, gamma=1.0):
     """ Iteratively evaluate the value-function under policy.
     Alternatively, we could formulate a set of linear equations in iterms of v[s] 
     and solve them to find the value function.
     """
-    v = np.zeros(env.nS)
+    v = np.zeros(env.observation_space.n)
     eps = 1e-10
     while True:
         prev_v = np.copy(v)
-        for s in range(env.nS):
+        for s in range(env.observation_space.n):
             policy_a = policy[s]
-            v[s] = sum([p * (r + gamma * prev_v[s_]) for p, s_, r, _ in env.P[s][policy_a]])
+            v[s] = sum([p * (r + gamma * prev_v[s_]) for p, s_, r, _ in env.env.P[s][policy_a]])
         if (np.sum((np.fabs(prev_v - v))) <= eps):
             # value converged
             break
@@ -56,12 +56,12 @@ def compute_policy_v(env, policy, gamma=1.0):
 
 def policy_iteration(env, gamma = 1.0):
     """ Policy-Iteration algorithm """
-    policy = np.random.choice(env.nA, size=(env.nS))  # initialize a random policy
+    policy = np.random.choice(env.action_space.n, size=(env.observation_space.n))  # initialize a random policy
     max_iterations = 200000
     gamma = 1.0
     for i in range(max_iterations):
-        old_policy_v = compute_policy_v(env, policy, gamma)
-        new_policy = extract_policy(old_policy_v, gamma)
+        old_policy_v = evaluate_policy_v(env, policy, gamma)
+        new_policy = improve_policy(old_policy_v, gamma)
         if (np.all(policy == new_policy)):
             print ('Policy-Iteration converged at step %d.' %(i+1))
             break
